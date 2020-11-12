067c50711bb59318d5fb12b14af6cdf82bdc99d5
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-12 17:08:50 +0100

    fseek function, set the poinnter to specific postion

---
+++ FileSystem/fseek.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fseek</title>
+</head>
+<body>
+    
+    <?php 
+        /*
+            fseek(resource, offset, [whence = SEEK_SET]) : 0 {success} | -1 
+            whence{
+                SEEK_SET => the default one, set the postion to the begining,
+                SEEK_CUR => set the postion to the currecnt one,
+                SEEK_END => set the postion to the enf of the file
+            }
+        */
+
+        // TEST THE "fseek" FUNCTION
+
+        echo "<h1>Test the \"fseek\" function !!</h1>";
+
+        $fileName = "Read.txt";
+
+        $resource = fopen($fileName, "r+"); // OPENING THE FILE
+
+        echo "<h3>Content BEFORE function's call : '" . fread($resource, filesize($fileName)) . "'</h3>";
+
+        fseek($resource, 3); // PUT THE POINTER TO THE NUMBER 3 POSTION
+
+        fwrite($resource, "A");
+
+        fseek($resource, 0); // RESET THE POSTION TO THE BEGINING
+
+        echo "<h3>Content AFTER function's call : '" . fread($resource, filesize($fileName)) . "'</h3>";
+    ?>
+</body>
+</html>
--- FileSystem/fwrite.php
+++ FileSystem/fwrite.php
@@ -15,13 +15,13 @@
             $file = "Read.txt";
             $resource = fopen($file, "r+");
 
-            $content = fread($resource, filesize($file));
+            //$content = fread($resource, filesize($file));
 
             echo "<h1>Test the \"fwrite\" Fuction</h1>";
 
-            echo "<h3>File Content : '" . $content . "'</h3>";
+            //echo "<h3>File Content : '" . $content . "'</h3>";
 
-            fwrite($resource, ", THIS DATA WAS ADDED BY 'fwrite' FUNCTION");
+            fwrite($resource, ", 20");
         ?>
 </body>
 </html>
