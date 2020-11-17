8b7bebecdbfad9d56340c7b2e486caddd1b99602
zakaria ben salek <zakariaazaraf@gmail.com>
2020-11-17 23:52:26 +0100

    how to connect to database using PDO extension

---
+++ connectDatabase/connect.php
@@ -0,0 +1,49 @@
+<?php
+
+    // CONNECT TO DATABASE USING "PDO" EXTENSION
+
+
+    $databaseSourceName = 'mysql:host=localhost;dbname=ziko_database_test';
+    $dbn = 'mysql:host=localhost;dbname=ziko_database_test';
+
+    $user = 'root';
+
+    $password = '';
+    $pass = '';
+
+   /*  $options = array(
+        PDO::MYSQL_ATRR_INIT_COMMAND => 'SET NAMES utf8', // SET THE CHARSET
+    ); */
+
+    // START TESTING THE CONNECTION
+    echo "<h1>Test The Connection To Databases With mysql Using PDO extension</h1>";
+
+    try{
+
+        $db = new PDO($dbn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "<h2>Connected Successfully To Database !!</h2>";
+
+
+        // INSERT A QUERY TO DATABASE
+        $q = "insert into products values (NULL, 'WAC')";
+
+        // EXECUTE THE QUERY
+        $db->exec($q);
+
+        // HOW TO GET DATA FROM THE DATABASE
+        foreach($db->query('select * from products') as $row){
+
+            echo $row[0] . " " . $row[1] . "<br>";
+
+            /* echo $row['id'] . " " . $row['productName'] . "<br>"; */
+
+        }
+
+    }catch(PDOException $e){
+
+        echo "<h2>Failed To Connect To Database .<br>" . $e->getMessage() . "</h2>";
+
+    }
